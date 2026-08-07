4fc5a2cc6a17eb1bdef2554a75c09d531de0030d
veerdata00-alt <veerdata00@gmail.com>
2026-08-07 20:27:21 +0530

    deploy: sync scratch_apps2.js from local

---
+++ scratch_apps2.js
@@ -0,0 +1,14 @@
+const axios = require('axios');
+const appId = "1746396296328706";
+const appSecret = "976f1dd407381d2ef79162db49aefd78";
+const appToken = `${appId}|${appSecret}`;
+
+async function getAppWebhooks() {
+    try {
+        const webhooks = await axios.get(`https://graph.facebook.com/v19.0/${appId}/subscriptions?access_token=${appToken}`);
+        console.log("App Webhook Subscriptions:", JSON.stringify(webhooks.data, null, 2));
+    } catch (e) {
+        console.error("Error:", e.response ? e.response.data : e.message);
+    }
+}
+getAppWebhooks();
