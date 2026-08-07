9b79488a7a1daac8bc879f9fcc8e2ae21611f152
veerdata00-alt <veerdata00@gmail.com>
2026-08-07 20:22:54 +0530

    deploy: sync campaigns_._id-B8sRamD5.js from local

---
+++ frontend/dist/server/assets/campaigns_._id-B8sRamD5.js
@@ -0,0 +1,6 @@
+import { createFileRoute, lazyRouteComponent } from "@tanstack/react-router";
+//#region src/routes/campaigns_.$id.tsx
+var $$splitComponentImporter = () => import("./campaigns_._id-CMwFE_aM.js");
+var Route = createFileRoute("/campaigns_/$id")({ component: lazyRouteComponent($$splitComponentImporter, "component") });
+//#endregion
+export { Route as t };
