d279fabd5c014c869579eee54a065ca14a4ebbe8
veerdata00-alt <veerdata00@gmail.com>
2026-08-07 20:27:14 +0530

    deploy: sync replace_openwizard.js from local

---
+++ replace_openwizard.js
@@ -0,0 +1,6 @@
+﻿const fs = require('fs');
+let code = fs.readFileSync('public/dashboard.js', 'utf8');
+
+code = code.replace(/window\.openWizard\(\)/g, "window.switchTab('builder')");
+
+fs.writeFileSync('public/dashboard.js', code, 'utf8');
