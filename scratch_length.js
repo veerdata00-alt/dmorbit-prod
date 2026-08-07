fa74ded3b67785c2fb39f6e78b324bb227e2260e
veerdata00-alt <veerdata00@gmail.com>
2026-08-07 20:27:34 +0530

    deploy: sync scratch_length.js from local

---
+++ scratch_length.js
@@ -0,0 +1,2 @@
+const p = '{"object":"instagram","entry":[{"time":1784201623398,"id":"17841421930215757","messaging":[{"timestamp":1784200184302,"read":{"mid":"aWdfZAG1faXRlbToxOklHTWVzc2FnZAUlEOjE3ODQxNDIxOTMwMjE1NzU3OjM0MDI4MjM2Njg0MTcxMDMwMTI0NDI3NjI3MDQyNzMzODYwMzc1NTozMjkxMjY4NTE1Mzk3MDQwMzA2MTE4ODU3Nzc3MDYwMjQ5NgZDZD"}}]}]}';
+console.log('Length:', Buffer.byteLength(p));
