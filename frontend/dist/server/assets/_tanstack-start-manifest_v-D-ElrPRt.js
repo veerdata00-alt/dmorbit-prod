79bfa7427ecbd733f867f2b270c0b755620f52f5
veerdata00-alt <veerdata00@gmail.com>
2026-08-07 20:23:32 +0530

    deploy: sync _tanstack-start-manifest_v-D-ElrPRt.js from local

---
+++ frontend/dist/server/assets/_tanstack-start-manifest_v-D-ElrPRt.js
@@ -0,0 +1,224 @@
+//#region \0tanstack-start-manifest:v
+var tsrStartManifest = () => ({ routes: {
+	__root__: {
+		filePath: "C:/Users/BBC IT HUB/DMorbit/frontend/src/routes/__root.tsx",
+		children: [
+			"/",
+			"/analytics",
+			"/billing",
+			"/campaigns",
+			"/crm",
+			"/forgot-password",
+			"/home",
+			"/login",
+			"/settings",
+			"/signup",
+			"/smart-bio",
+			"/welcome",
+			"/campaigns_/$id",
+			"/campaigns_/new"
+		],
+		preloads: [
+			"/assets/index-woHE_vzx.js",
+			"/assets/sparkles-BxGks-H3.js",
+			"/assets/link-CJ_wzbPD.js"
+		],
+		scripts: [{ attrs: {
+			type: "module",
+			async: !0,
+			src: "/assets/index-woHE_vzx.js"
+		} }]
+	},
+	"/": {
+		filePath: "C:/Users/BBC IT HUB/DMorbit/frontend/src/routes/index.tsx",
+		children: void 0,
+		preloads: [
+			"/assets/routes-a_rUZWNy.js",
+			"/assets/arrow-right-Utw3qIyI.js",
+			"/assets/reply-BVemb2ni.js",
+			"/assets/chart-column-Cfs_a4LJ.js",
+			"/assets/check-BpOkt8TT.js",
+			"/assets/instagram-WFalL7Y7.js",
+			"/assets/message-circle-B5AApFB0.js",
+			"/assets/play-VbCUwN0o.js",
+			"/assets/users-aKBaXQez.js",
+			"/assets/zap-BLgTuIye.js"
+		]
+	},
+	"/analytics": {
+		filePath: "C:/Users/BBC IT HUB/DMorbit/frontend/src/routes/analytics.tsx",
+		children: void 0,
+		preloads: [
+			"/assets/analytics-DFp_9des.js",
+			"/assets/useQuery-DJaCS2GR.js",
+			"/assets/AppShell-Bit3TOe8.js",
+			"/assets/chart-column-Cfs_a4LJ.js",
+			"/assets/message-circle-B5AApFB0.js",
+			"/assets/send-BPv5cmAx.js",
+			"/assets/users-aKBaXQez.js"
+		]
+	},
+	"/billing": {
+		filePath: "C:/Users/BBC IT HUB/DMorbit/frontend/src/routes/billing.tsx",
+		children: void 0,
+		preloads: [
+			"/assets/billing-CTPwaJ3B.js",
+			"/assets/AppShell-Bit3TOe8.js",
+			"/assets/check-BpOkt8TT.js",
+			"/assets/download-D-q5Wxiw.js"
+		]
+	},
+	"/campaigns": {
+		filePath: "C:/Users/BBC IT HUB/DMorbit/frontend/src/routes/campaigns.tsx",
+		children: void 0,
+		preloads: [
+			"/assets/campaigns-BLldmXnz.js",
+			"/assets/dist-CnN2wgM-.js",
+			"/assets/pause-CXW_sgY1.js",
+			"/assets/useQuery-DJaCS2GR.js",
+			"/assets/AppShell-Bit3TOe8.js",
+			"/assets/check-BpOkt8TT.js",
+			"/assets/chevron-right-CRDZU1oh.js",
+			"/assets/megaphone-Cndo7IRQ.js",
+			"/assets/play-VbCUwN0o.js",
+			"/assets/plus-BRrAzHxD.js",
+			"/assets/search-Cuv1-pc2.js",
+			"/assets/trash-2-Ddvs-CT6.js"
+		]
+	},
+	"/crm": {
+		filePath: "C:/Users/BBC IT HUB/DMorbit/frontend/src/routes/crm.tsx",
+		children: void 0,
+		preloads: [
+			"/assets/crm-CGpknwYQ.js",
+			"/assets/useQuery-DJaCS2GR.js",
+			"/assets/AppShell-Bit3TOe8.js",
+			"/assets/mail-0AiVetam.js",
+			"/assets/search-Cuv1-pc2.js"
+		]
+	},
+	"/forgot-password": {
+		filePath: "C:/Users/BBC IT HUB/DMorbit/frontend/src/routes/forgot-password.tsx",
+		children: void 0,
+		preloads: [
+			"/assets/forgot-password-C6mJU3aw.js",
+			"/assets/arrow-right-Utw3qIyI.js",
+			"/assets/circle-check-hn40YkUb.js",
+			"/assets/mail-0AiVetam.js"
+		]
+	},
+	"/home": {
+		filePath: "C:/Users/BBC IT HUB/DMorbit/frontend/src/routes/home.tsx",
+		children: void 0,
+		preloads: [
+			"/assets/home-BvN58r0u.js",
+			"/assets/useQuery-DJaCS2GR.js",
+			"/assets/AppShell-Bit3TOe8.js",
+			"/assets/arrow-right-Utw3qIyI.js",
+			"/assets/chart-column-Cfs_a4LJ.js",
+			"/assets/instagram-WFalL7Y7.js",
+			"/assets/lock-Uolk7uUi.js",
+			"/assets/megaphone-Cndo7IRQ.js",
+			"/assets/message-circle-B5AApFB0.js",
+			"/assets/plus-BRrAzHxD.js",
+			"/assets/trending-up-BjZUoblJ.js",
+			"/assets/users-aKBaXQez.js"
+		]
+	},
+	"/login": {
+		filePath: "C:/Users/BBC IT HUB/DMorbit/frontend/src/routes/login.tsx",
+		children: void 0,
+		preloads: [
+			"/assets/login-BjIgYIg8.js",
+			"/assets/arrow-right-Utw3qIyI.js",
+			"/assets/eye-DJ6P473W.js",
+			"/assets/lock-Uolk7uUi.js",
+			"/assets/mail-0AiVetam.js"
+		]
+	},
+	"/settings": {
+		filePath: "C:/Users/BBC IT HUB/DMorbit/frontend/src/routes/settings.tsx",
+		children: void 0,
+		preloads: [
+			"/assets/settings-ZM95l88I.js",
+			"/assets/AppShell-Bit3TOe8.js",
+			"/assets/chevron-right-CRDZU1oh.js",
+			"/assets/instagram-WFalL7Y7.js",
+			"/assets/user-D40VYYo4.js",
+			"/assets/dialog-BsxVT3jo.js"
+		]
+	},
+	"/signup": {
+		filePath: "C:/Users/BBC IT HUB/DMorbit/frontend/src/routes/signup.tsx",
+		children: void 0,
+		preloads: [
+			"/assets/signup-D_eeW1OE.js",
+			"/assets/arrow-right-Utw3qIyI.js",
+			"/assets/check-BpOkt8TT.js",
+			"/assets/eye-DJ6P473W.js",
+			"/assets/lock-Uolk7uUi.js",
+			"/assets/mail-0AiVetam.js",
+			"/assets/user-D40VYYo4.js"
+		]
+	},
+	"/smart-bio": {
+		filePath: "C:/Users/BBC IT HUB/DMorbit/frontend/src/routes/smart-bio.tsx",
+		children: void 0,
+		preloads: [
+			"/assets/smart-bio-CV_IGZmF.js",
+			"/assets/AppShell-Bit3TOe8.js",
+			"/assets/chart-column-Cfs_a4LJ.js",
+			"/assets/plus-BRrAzHxD.js",
+			"/assets/trash-2-Ddvs-CT6.js"
+		]
+	},
+	"/welcome": {
+		filePath: "C:/Users/BBC IT HUB/DMorbit/frontend/src/routes/welcome.tsx",
+		children: void 0,
+		preloads: [
+			"/assets/welcome-BEpFUiTi.js",
+			"/assets/instagram-WFalL7Y7.js",
+			"/assets/message-circle-B5AApFB0.js",
+			"/assets/trending-up-BjZUoblJ.js",
+			"/assets/zap-BLgTuIye.js"
+		]
+	},
+	"/campaigns_/$id": {
+		filePath: "C:/Users/BBC IT HUB/DMorbit/frontend/src/routes/campaigns_.$id.tsx",
+		children: void 0,
+		preloads: [
+			"/assets/campaigns_._id-Dj-ttoMj.js",
+			"/assets/pause-CXW_sgY1.js",
+			"/assets/useQuery-DJaCS2GR.js",
+			"/assets/AppShell-Bit3TOe8.js",
+			"/assets/arrow-left-Ctz130jt.js",
+			"/assets/download-D-q5Wxiw.js",
+			"/assets/megaphone-Cndo7IRQ.js",
+			"/assets/message-circle-B5AApFB0.js",
+			"/assets/play-VbCUwN0o.js",
+			"/assets/send-BPv5cmAx.js",
+			"/assets/trash-2-Ddvs-CT6.js",
+			"/assets/dialog-BsxVT3jo.js"
+		]
+	},
+	"/campaigns_/new": {
+		filePath: "C:/Users/BBC IT HUB/DMorbit/frontend/src/routes/campaigns_.new.tsx",
+		children: void 0,
+		preloads: [
+			"/assets/campaigns_.new-CeCCr1KJ.js",
+			"/assets/useQuery-DJaCS2GR.js",
+			"/assets/arrow-left-Ctz130jt.js",
+			"/assets/arrow-right-Utw3qIyI.js",
+			"/assets/reply-BVemb2ni.js",
+			"/assets/check-BpOkt8TT.js",
+			"/assets/circle-check-hn40YkUb.js",
+			"/assets/megaphone-Cndo7IRQ.js",
+			"/assets/message-circle-B5AApFB0.js",
+			"/assets/search-Cuv1-pc2.js",
+			"/assets/users-aKBaXQez.js",
+			"/assets/x-CtuzErAW.js"
+		]
+	}
+} });
+//#endregion
+export { tsrStartManifest };
