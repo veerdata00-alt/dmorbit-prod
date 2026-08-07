c2f812dd692986667f837806d32ed65259ef5684
veerdata00-alt <veerdata00@gmail.com>
2026-08-07 20:21:12 +0530

    deploy: sync audit_user.js from local

---
+++ audit_user.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+async function run() {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        const db = mongoose.connection.db;
+        const User = mongoose.connection.collection('users');
+        const user = await User.find({ email: 'veerdata00@gmail.com' }).toArray();
+        console.log(JSON.stringify(user, null, 2));
+    } catch(err) {
+        console.error(err);
+    } finally {
+        await mongoose.disconnect();
+    }
+}
+run();
